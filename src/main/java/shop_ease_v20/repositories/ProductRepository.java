9ed6be521e5948f9e211ea1ab157c9f82382d6ba
login-nigol <vadim.antipov@gmx.de>
2024-09-24 08:47:28 +0200

    Добавлены репозитории

--- src/main/java/shop_ease_v20/repositories/ProductRepository.java
+++ src/main/java/shop_ease_v20/repositories/ProductRepository.java
@@ -8,6 +8,19 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByproductNameContainingIgnoreCase(String name);
+    List<Product> findByproductNameContainingIgnoreCase(String productName);
 }
 
+// @Repository — это аннотация Spring, которая указывает, что интерфейс или класс является репозиторием (DAO)
+// для работы с базой данных.
+//
+// public interface ProductRepository extends JpaRepository<Product, Long> — интерфейс ProductRepository наследует
+// JpaRepository, где:
+//
+// Product — это сущность (Entity), с которой работает репозиторий.
+// Long — это тип данных идентификатора сущности Product (обычно это поле id).
+// List<Product> findByProductNameContainingIgnoreCase(String name) — это метод, который автоматически реализуется
+// Spring Data JPA. Он позволяет найти список продуктов, у которых имя (productName) содержит строку name без учета регистра:
+//
+//Containing — означает, что поиск будет по части строки (аналог SQL-оператора LIKE).
+//IgnoreCase — поиск будет проводиться без учета регистра символов.
